29af104ce3274993edd08bb6e94a4b3aea2ae13a
JRMartinez <johnny.martinez@outlook.com>
2023-04-01 02:26:07 -0400

    Finish date class POO

---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConceptosPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+Date date1 = new Date(22, 3, 1974);
+
+
+try
+{
+    Console.WriteLine(new Date(29, 2, 2024));
+    Console.WriteLine(new Date(31, 9, 2022));
+    Console.WriteLine(new Date(5, 8, 1974));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
